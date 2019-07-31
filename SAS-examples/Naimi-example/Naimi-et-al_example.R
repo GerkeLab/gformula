93bcf1272f3986aaa9afe2657a1104037e2fac65
Travis Gerke <tgerke@hsph.harvard.edu>
2019-07-31 12:48:28 -0400

    complete modeling step

--- SAS-examples/Naimi-example/Naimi-et-al_example.R
+++ SAS-examples/Naimi-example/Naimi-et-al_example.R
@@ -19,7 +19,7 @@ raw_data <-
 
 naimi <- read_delim(raw_data, col_names = c("a0", "z1", "a1", "y", "n"), delim = " ")
 
-naimi %>%
+naimi <- naimi %>%
   rename(y2 = y) %>%
   mutate(row = row_number()) %>%
   nest(data = c(-n, -row)) %>%
@@ -35,3 +35,42 @@ naimi %>%
   select(-z1) %>%
   mutate_at(vars(a, z), as.integer)
 
+# Estimate parameters -----------------------------------------------------
+
+# z1 is caused by a0
+z1_fit <- glm(z_1 ~ a_0,
+              family = "binomial",
+              data = naimi %>%
+                filter(time %in% 0:1) %>%
+                select(-y) %>%
+                pivot_wider(id_cols = id, names_from = time, values_from = c(a, z)) %>%
+                select(-a_1, -z_0)
+              )
+# from the SAS output in the supplement, this should return
+# beta_0 = -0.4309 and beta_1 = 0.8735
+coef(z1_fit)
+
+# a1 is caused by z1 (and *not* a0; we read this from the DAG. This
+# is an important option to consider: we don't always use all previously
+# measured variables to predict future variables)
+a1_fit <- glm(a_1 ~ z_1,
+              family = "binomial",
+              data = naimi %>%
+                filter(time == 1) %>%
+                select(-y) %>%
+                pivot_wider(id_cols = id, names_from = time, values_from = c(a, z))
+              )
+# from the SAS output in the supplement, this should return
+# beta_0 = -0.8002 and beta_1 = 1.6084
+coef(a1_fit)
+
+# y is caused by a1, z1, a1, *and* an a0*a1 interaction
+# another important option to include: permit variable interactions
+y_fit <- lm(y_2 ~ a_0 + z_1 + a_1 + a_0*a_1,
+              data = naimi %>%
+                pivot_wider(id_cols = id, names_from = time, values_from = c(a, z, y)) %>%
+                select(-a_2, -z_0, -z_2, -y_0, -y_1)
+          )
+# from supplement:
+# 87.2466 + 24.9699*a1 + 32.5502*z1 + 17.9893*a0 + 0.0541*a1*a0
+coef(y_fit)
